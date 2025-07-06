8c69c9ae9b619aeb220fa24bc580d3949cf18669
cmcd1091 <cmcd1091@gmail.com>
2025-07-06 14:17:57 -0400

    Added parser

--- src/Components/CopiedItem.jsx
+++ src/Components/CopiedItem.jsx
@@ -1,12 +1,17 @@
 const CopiedItem = ({ file, selectedStore }) => {
-  const fileWithoutExt = file.replace('.png', '');
-  // console.log(fileWithoutExt)
-  const splitIndex = fileWithoutExt.lastIndexOf('-CN-');
-  // console.log("split index:", splitIndex)
-  const skuPart = fileWithoutExt.slice(0, splitIndex);
-  // console.log("skuPart:", skuPart);
-  const orderPart = fileWithoutExt.slice(splitIndex + 1)
-  // console.log("orderPart:", orderPart)
+  const parseFileName = (file) => {
+    const fileWithoutExt = file.replace('.png', '');
+    // console.log(fileWithoutExt)
+    const splitIndex = fileWithoutExt.lastIndexOf('-CN-');
+    // console.log("split index:", splitIndex)
+    const skuPart = fileWithoutExt.slice(0, splitIndex);
+    // console.log("skuPart:", skuPart);
+    const orderPart = fileWithoutExt.slice(splitIndex + 1)
+    // console.log("orderPart:", orderPart)
+    return { skuPart, orderPart };
+  }
+  
+  const { skuPart, orderPart } = parseFileName(file)
 
   return (
     <>
