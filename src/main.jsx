f7e7eab40eabc5e41e3243eb9fdff34f0b471108
cmcd1091 <cmcd1091@gmail.com>
2025-12-10 22:31:12 -0500

    Updated routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "../frontend/app/components/App";
-import { AppProvider } from "./context/AppContext";
-import "./Styling/App.css";
+import { AppProvider } from "../frontend/app/context/AppContext.jsx";
+import "../frontend/app/styles/App.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
